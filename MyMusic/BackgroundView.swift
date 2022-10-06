b42eb4fa76264ab3705ec7993d327fb6b9ac9a29
小泉孝雄 <takao_koizumi@backapp.co.jp>
2022-10-06 18:54:02 +0900

    リファクタリング前

---
+++ MyMusic/BackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundView.swift
+//  MyMusic
+//
+//  Created by Takao Koizumi on 2022/10/06.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
---
+++ MyMusic/SoundPlayer.swift
@@ -0,0 +1,12 @@
+//
+//  SoundPlayer.swift
+//  MyMusic
+//
+//  Created by Takao Koizumi on 2022/10/06.
+//
+
+import UIKit
+
+class SoundPlayer: NSObject {
+
+}
