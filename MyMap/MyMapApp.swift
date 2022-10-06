718fd8825dcd7d528ef14476adb0be8d13156829
小泉孝雄 <takao_koizumi@backapp.co.jp>
2022-10-06 19:39:09 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Takao Koizumi on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
