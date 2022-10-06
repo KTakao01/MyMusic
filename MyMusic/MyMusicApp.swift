b8b3b8792fe6ff26d19691870589b10da6ce34c0
小泉孝雄 <takao_koizumi@backapp.co.jp>
2022-10-06 10:59:37 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Takao Koizumi on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
