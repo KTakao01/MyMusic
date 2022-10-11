2530cb582b339453b2360a0244b9137640e5da6c
小泉孝雄 <takao_koizumi@backapp.co.jp>
2022-10-11 20:32:31 +0900

    pin

--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -6,15 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    
+    let searchKey: String
+    
+    func makeUIView(context: Context)-> MKMapView{
+        MKMapView()
     }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+        print(searchKey)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchKey, completionHandler: {(placemarks,error)in
+            
+            if let unwrapPlacemarks = placemarks ,
+               let firstPlacemark = unwrapPlacemarks.first ,
+               let location = firstPlacemark.location {
+                
+                let targetCoordinate = location.coordinate
+                
+                print(targetCoordinate)
+                
+                let pin = MKPointAnnotation()
+                
+                pin.coordinate = targetCoordinate
+                
+                pin.title = searchKey
+                
+                uiView.addAnnotation(pin)
+                
+                uiView.region = MKCoordinateRegion(
+                    center: targetCoordinate,
+                    latitudinalMeters: 500.0,
+                    longitudinalMeters: 500.0)
+            }
+        })
+    }
+    
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(searchKey: "東京タワー")
     }
 }
